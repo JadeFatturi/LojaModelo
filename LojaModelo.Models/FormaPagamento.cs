22dd2e9ad4dfc7e92878f027f682c135f8fad2d7
JadeFatturi <jade.fatturi@hotmail.com>
2023-11-09 15:10:54 -0300

    Adicionar arquivos de projeto.

---
+++ LojaModelo.Models/FormaPagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaModelo.Models
+{
+    public enum FormaPagamento
+    {
+        Dinheiro=1,
+        Cartao=2,
+        Boleto=3,
+        Deposito=4,
+        Pix=5,
+    }
+}
---
+++ LojaModelo.Models/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaModelo.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<Item> Items { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+        public class Item
+        {
+            public int Ordem { get; set; }
+            public Produto Produto { get; set; }
+            public int Quantidade { get; set; }
+            public decimal Preco { get; set; }
+        }
+    }
+}
