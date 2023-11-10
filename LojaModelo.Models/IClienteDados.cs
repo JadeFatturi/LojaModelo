bbcb3488fec58b9ecde929eb3a4f233ba4a6c64a
JadeFatturi <jade.fatturi@hotmail.com>
2023-11-09 23:46:23 -0300

    Criação da classe do projeto DAL com ações de alterar, excluir, incluir e obter informações de pedidos dos clientes por: e-mail, Id e listar todos os clientes.

---
+++ LojaModelo.Models/IClienteDados.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaModelo.Models
+{
+    public interface IClienteDados
+    {
+        void Incluir(Cliente cliente);
+        void Alterar(Cliente cliente);
+        void Excluir(string Id);
+        List<Cliente> ObterTodos();
+        Cliente ObterPorId(string id);
+        Cliente ObterPorEmail(string email);
+
+
+
+    }
+}
